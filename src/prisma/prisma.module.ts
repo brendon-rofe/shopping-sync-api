1b6857b1f9744882620042c112f1847ef733015a
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-17 08:22:27 +0200

    fix: Import PrismaClient and PrismaService correctly

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma.service";
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [PrismaModule],
+  providers: [PrismaService],
 })
 export class PrismaModule {}
