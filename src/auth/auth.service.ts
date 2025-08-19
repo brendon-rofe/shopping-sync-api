3a1ab34100da85c87cd13b09db0c2162b849988a
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-19 09:27:15 +0200

    feat: Create initial auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
+
+
+@Injectable()
+export class AuthService {
+  constructor() {}
+  private readonly saltRounds = 10;
+
+  async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, this.saltRounds);
+  }
+
+}
