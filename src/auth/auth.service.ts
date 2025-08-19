d63d59bbb45254517e6e1d64794e34a5cb0c97f3
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-19 09:51:28 +0200

    feat: Create comparePasswords method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,4 +11,7 @@ export class AuthService {
     return await bcrypt.hash(password, this.saltRounds);
   }
 
+  async comparePasswords(password: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(password, hash);
+  }
 }
