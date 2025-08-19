e68de64f086fb818971cc32185fb7496220c8829
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-19 08:41:09 +0200

    feat: Create and export/import auth.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     PrismaModule, 
-    UserModule
+    UserModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
