b0ffae745d4b4d05b5727a6148c6f1e68034ca19
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-19 13:03:10 +0200

    feat: Import JWT module and set it up in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { forwardRef, Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { UserModule } from "src/user/user.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [forwardRef(() => UserModule)],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '15m' },
+    }),
+    forwardRef(() => UserModule)
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
