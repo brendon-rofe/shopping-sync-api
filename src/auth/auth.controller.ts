c4706eaea77ef39f38bdbcde1c668e0b066d029b
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-20 10:30:51 +0200

    feat: Implement functionality to get jwt token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,16 @@
-import { Body, Controller, Post, UnauthorizedException } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/auth.dtos";
-import { UserService } from "src/user/user.service";
 
 @Controller()
 export class AuthController{
   constructor(
     private readonly authService: AuthService,
-    private readonly userService: UserService,
   ) {}
 
   @Post("auth/login")
   async login(@Body() dto: LoginDto) {
-    const user = await this.userService.findByEmail(dto.email);
-    const isPasswordValid = await this.authService.comparePasswords(dto.password, user.password);
-    if (!isPasswordValid) {
-      throw new UnauthorizedException('Password incorrect');
-    }
-    return { message: "Login successful" };
+    return await this.authService.login(dto);
   }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from "./dto/auth.dtos";
+import { UserService } from "src/user/user.service";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async comparePasswords(password: string, hash: string): Promise<boolean> {
     return await bcrypt.compare(password, hash);
   }
+
+  async login(dto: LoginDto) {
+    const user = await this.userService.findByEmail(dto.email);
+
+    const ok = await this.comparePasswords(dto.password, user.password);
+    if(!ok) {
+      throw new UnauthorizedException('Invalid password');
+    }
+    const payload = { sub: user.id, email: user.email };
+    const accessToken = await this.jwtService.signAsync(payload);
+    return { accessToken };
+  }
+
 }
