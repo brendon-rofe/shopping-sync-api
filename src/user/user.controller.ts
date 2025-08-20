7c57b79f7e9d4b221f987f73995307288c7f9588
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-20 12:11:34 +0200

    fix: Changing using guards for specific endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,6 @@ import { UserService } from "./user.service";
 import { CreateUserDto, UpdateUserEmailDto } from "./dto/user.dtos";
 import { JwtAuthGuard } from "src/auth/jwt.auth-guard";
 
-@UseGuards(JwtAuthGuard)
 @Controller("/user")
 export class UserController {
   constructor (private readonly userService: UserService) {}
@@ -13,16 +12,19 @@ export class UserController {
     return await this.userService.create(user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get("/:email")
   async findByEmail(@Param("email") email: string) {
     return await this.userService.findByEmail(email);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put("/user/:email")
   async updateUser(@Param("email") email: string, @Body() password: UpdateUserEmailDto) {
     return await this.userService.updateUserPassword(email, password.password)
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete("/user/:email")
   async deleteUser(@Param("email") email: string) {
     return await this.userService.deleteUser(email)
