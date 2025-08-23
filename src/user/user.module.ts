4aa365b23e1283e50a604267c8bd539018a47268
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-23 17:29:02 +0200

    feat: Update

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,6 @@ import { Module } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 import { PrismaModule } from "src/prisma/prisma.module";
-import { AuthModule } from "src/auth/auth.module";
 
 @Module({
   imports: [PrismaModule],
