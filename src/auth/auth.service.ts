1222e4503c90178a4aa12f38eee3952ed020b975
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-25 12:49:44 +0200

    fix: Revert back to only jwt return

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -25,10 +25,7 @@ export class AuthService {
     }
     const payload = { sub: user.id, email: user.email };
     const accessToken = await this.jwtService.signAsync(payload);
-    return { 
-      id: user.id,
-      accessToken
-    };
+    return { accessToken };
   }
 
 }
