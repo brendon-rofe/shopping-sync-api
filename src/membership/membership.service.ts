1fe6d2b1f8bc05ec2aebf74678dbaf2a674dc0ba
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-09-09 07:07:24 +0200

    feat: Create initial get by ID method

--- src/membership/membership.service.ts
+++ src/membership/membership.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class MembershipService {}
+export class MembershipService {
+  constructor(private prisma: PrismaService) {}
+
+  async getById(id: string) {
+    const membershipId = Number(id)
+    await this.prisma.membership.findFirst({
+        where: {
+          id: membershipId,
+        },
+      }
+    )
+  }
+
+}
